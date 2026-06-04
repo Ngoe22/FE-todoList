487a821cb7b4abbf1c73d8b480450338b87e8740
Finn <ngoeee0402@gmail.com>
2026-06-04 23:50:22 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+      <>
+          <div>Anh Cường: Phụ trách phần app/src/projects</div>
+          <div>Bạn Cường: Phụ trách phần  API</div>
+          <div>Thái Sơn: Phụ trách phần app/src/tickets</div>
+          <hr/>
+          <h2>Lưu ý chung:</h2>
+          <div>Sử dụng lệnh docker compose up để khởi chạy dự án tại localhost:3000.</div>
+          <div>Thống nhất sử dụng Tailwind CSS cho toàn bộ dự án.</div>
+      </>
+  );
+}
