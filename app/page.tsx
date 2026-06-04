697e93a5dfbb44a963651f856b943faf084c3515
Finn <ngoeee0402@gmail.com>
2026-06-05 00:07:46 +0700

    12

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <h2>Lưu ý chung:</h2>
           <div>Sử dụng lệnh docker compose up để khởi chạy dự án tại localhost:3000.</div>
           <div>Thống nhất sử dụng Tailwind CSS cho toàn bộ dự án.</div>
-          <div>Cam on rat nhieu</div>
+          <div>Cam on rat nhieu test</div>
       </>
   );
 }
